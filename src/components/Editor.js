d094729c51c096c1585045b89768fa7eebaa2837
vishalkrsharma <vishalkrsharma1234@gmail.com>
2022-08-07 21:50:56 +0530

    code change error

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -5,9 +5,11 @@ import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/monokai.css';
 import 'codemirror/addon/edit/closetag';
 import 'codemirror/addon/edit/closebrackets';
-// import ACTIONS from '../Actions';
 
-function Editor() {
+import ACTIONS from '../Actions';
+import { Socket } from 'socket.io-client';
+
+function Editor({ socketRef, roomId }) {
   const editorRef = useRef(null);
   useEffect(() => {
     async function init() {
@@ -18,9 +20,34 @@ function Editor() {
         autoCloseBrackets: true,
         lineNumbers: true,
       });
+
+      editorRef.current.on('change', (instance, changes) => {
+        console.log('changes', changes);
+        const { orgin } = changes;
+        const code = instance.getValue();
+
+        if (origin !== 'setValue') {
+          socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+            roomId,
+            code,
+          });
+        }
+
+        console.log(code);
+      });
     }
     init();
   }, []);
+
+  useEffect(() => {
+    if (socketRef.current) {
+      socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+        if (code !== null) {
+          editorRef.current.setValue(code);
+        }
+      });
+    }
+  }, [socketRef.current]);
 
   return <textarea id='realtimeEditor'></textarea>;
 }
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,37 +1,84 @@
 import React, { useState, useRef, useEffect } from 'react';
+import toast from 'react-hot-toast';
 import ACTIONS from '../Actions';
-
 import Client from '../components/Client';
 import Editor from '../components/Editor';
 import { initSocket } from '../socket';
-
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate, Navigate, useParams } from 'react-router-dom';
 
 import '../styles/EditorPage.scss';
 
 function EditorPage() {
   const socketRef = useRef(null);
+  const codeRef = useRef(null);
   const location = useLocation();
+  const { roomId } = useParams();
+  const reactNavigator = useNavigate();
+  const [clients, setClients] = useState([]);
 
   useEffect(() => {
     const init = async () => {
       socketRef.current = await initSocket();
+      socketRef.current.on('connect_error', (err) => handleErrors(err));
+      socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log('socket error', e);
+        toast.error('Socket connection failed, try again later.');
+        reactNavigator('/');
+      }
+
       socketRef.current.emit(ACTIONS.JOIN, {
         roomId,
         username: location.state?.username,
       });
+
+      // Listening for joined event
+      socketRef.current.on(ACTIONS.JOINED, ({ clients, username, socketId }) => {
+        if (username !== location.state?.username) {
+          toast.success(`${username} joined the room.`);
+          console.log(`${username} joined`);
+        }
+        setClients(clients);
+        socketRef.current.emit(ACTIONS.SYNC_CODE, {
+          code: codeRef.current,
+          socketId,
+        });
+      });
+
+      // Listening for disconnected
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+        toast.success(`${username} left the room.`);
+        setClients((prev) => {
+          return prev.filter((client) => client.socketId !== socketId);
+        });
+      });
+    };
+    init();
+    return () => {
+      socketRef.current.disconnect();
+      socketRef.current.off(ACTIONS.JOINED);
+      socketRef.current.off(ACTIONS.DISCONNECTED);
     };
   }, []);
 
-  const [clients, setClients] = useState([
-    { socketId: 1, username: 'Vishal S' },
-    { socketId: 2, username: 'Matt M' },
-    { socketId: 3, username: 'Matt M' },
-    { socketId: 4, username: 'Matt M' },
-    { socketId: 5, username: 'Matt M' },
-    { socketId: 6, username: 'Matt M' },
-    { socketId: 7, username: 'Matt M' },
-  ]);
+  async function copyRoomId() {
+    try {
+      await navigator.clipboard.writeText(roomId);
+      toast.success('Room ID copied');
+    } catch (err) {
+      toast.error('Could not copy the Room ID');
+      console.error(err);
+    }
+  }
+
+  function leaveRoom() {
+    reactNavigator('/');
+  }
+
+  if (!location.state) {
+    return <Navigate to='/' />;
+  }
 
   return (
     <div className='wrap'>
@@ -47,12 +94,14 @@ function EditorPage() {
           </div>
         </div>
         <div className='side__bottom'>
-          <div className='btn white'>Copy Room ID</div>
+          <div className='btn white' onClick={copyRoomId}>
+            Copy Room ID
+          </div>
           <div className='btn red'>Leave Room</div>
         </div>
       </div>
       <div className='textarea'>
-        <Editor />
+        <Editor socketRef={socketRef} roomId={roomId} />
       </div>
     </div>
   );
